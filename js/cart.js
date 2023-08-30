aa7dcc044b000226a7df1a74e64bdeb878131d10
HWoool <gksdnf24@naver.com>
2023-08-30 18:20:34 +0900

    JS 파일 수정

--- js/cart.js
+++ js/cart.js
@@ -3,16 +3,16 @@ console.log(cartLol);
 
 function carting() {
   if (cartLol.length) {
-    $(".cart_info p").text("");
+    $(".cart_Info p").text("");
     let total = 0;
-    let lolList = ` <table border="1">
-                      <colgroup>
-                        <col />
-                        <col />
-                        <col />
-                        <col />
-                        <col />
-                      </colgroup>
+    let lolList = `<table>
+                    <colgroup>
+                      <col />
+                      <col />
+                      <col />
+                      <col />
+                      <col />
+                    </colgroup>
                     <thead>
                       <th>상품이미지</th>
                       <th>상품정보</th>
@@ -23,16 +23,24 @@ function carting() {
     lolList += `<tbody>`;
     cartLol.forEach((value, index) => {
       lolList += `<tr>
-              <td><img src="./images/${value.image}" alt="${value.name}"></td>
-              <td>${value.name} <br> <span class="pp">${value.price}</span></td>
+      <td><img src="./images/JSON/${value.image}" alt="${value.name}"></td>
+      <td><span class="np">${
+        value.name
+      }</span> <br> <span class="pp"> ${value.price
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></td>
                 <td>
-                <button type="button" class="item_plus">+</button>
-                <input type="text" value="${
-                  value.quantity
-                }" autocomplete="off" class="item_input">
-                  <button type="button" class="item_minus">-</button>
+                <div class="price_pm">
+                  <button type="button" class="item__minus">-</button>
+                  <input type="text" value="${
+                    value.quantity
+                  }" autocomplete="off" class="item__input"/>
+                    <button type="button" class="item__plus">+</button>
+                </div>
                 </td>
-                  <td class="pTotal">${value.quantity * value.price}</td>
+                  <td class="pTotal">${(value.quantity * value.price)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
                   <td><button type="button" class="remove"><i class="fa-solid fa-xmark"></i></button></td>
                 </tr>`;
       total = total + value.quantity * value.price;
@@ -40,10 +48,110 @@ function carting() {
     lolList += `</tbody>
                 <tfoot>
                 <tr>
-                <td colspan="5">합계 : <span>${total}</span> </td>
+                <td colspan="5">결제금액 <i class="fa-solid fa-pause fa-rotate-90"></i>  <span class="total_span">${total
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span> </td>
                 </tr>
                 </tfoot>
                 </table>
-                <div class="buy_btn"><button type="text">주문하기</button></div>`;
+                <div class="buy_btn order"><button type="text">주문하기</button></div>`;
+    $(".cart_section table").remove();
+    $(".cart_section .order").remove();
+    $(".cart_section").append(lolList);
+  } else {
+    $(".cart_section table").remove();
+    $(".cart_section .order").remove();
+    $(".cart_Info p").text("장바구니에 상품이 없습니다.");
   }
 }
+carting();
+// 0~9까지 숫자를 제외한 모든 것(문자,공백 등)을 rep라는 변수에 넣어라
+let rep = /[^0-9]/g;
+
+// 장바구니 수량(+) 증가
+$("body").on("click", ".item__plus", function () {
+  let quantity = $(this).prev().val();
+  let mypp = $(this).parent().parent().prev().find(".pp").text();
+  mypp = mypp.replace(rep, "");
+  let total = 0;
+  if (quantity >= 1) {
+    quantity = parseInt(quantity);
+    $(this).prev().val(++quantity);
+    total = quantity * parseInt(mypp);
+  } else {
+    quantity = 1;
+    $(this).prev().val(quantity);
+    total = quantity * parseInt(mypp);
+  }
+  $(this)
+    .parent()
+    .parent()
+    .next()
+    .text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  loltotal();
+});
+// 장바구니 수량(-) 감소
+$("body").on("click", ".item__minus", function () {
+  let quantity = $(this).next().val();
+  let mypp = $(this).parent().parent().prev().find(".pp").text();
+  let total = 0;
+  mypp = mypp.replace(rep, "");
+  if (quantity > 1) {
+    quantity = parseInt(quantity);
+    $(this).next().val(--quantity);
+    total = quantity * parseInt(mypp);
+  } else {
+    quantity = 1;
+    $(this).next().val(quantity);
+    total = quantity * parseInt(mypp);
+  }
+  $(this)
+    .parent()
+    .parent()
+    .next()
+    .text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  loltotal();
+});
+
+// input 입력칸에 숫자
+$("body").on("keyup", ".item__input", function () {
+  let quantity = $(this).val();
+  let mypp = $(this).parent().parent().prev().find(".pp").text();
+  let total = 0;
+  mypp = mypp.replace(rep, "");
+  if (quantity) {
+    quantity = parseInt(quantity);
+    $(this).val(quantity);
+    total = quantity * parseInt(mypp);
+  } else {
+    quantity = "";
+    $(this).val(quantity);
+    total = quantity * parseInt(mypp);
+  }
+  $(this)
+    .parent()
+    .parent()
+    .next()
+    .text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  loltotal();
+});
+
+// 금액에 합계
+function loltotal() {
+  let totalLol = 0;
+  $("tbody .pTotal").each(function () {
+    let price = $(this).text();
+    price = price.replace(rep, "");
+    totalLol += parseInt(price);
+  });
+  $("tfoot .total_span").text(
+    totalLol.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  );
+}
+// 선택항목에 삭제버튼
+$("body").on("click", "tbody .remove", function () {
+  let lolnum = $(this).parent().parent().index();
+  cartLol.splice(lolnum, 1);
+  localStorage.setItem("lolItem", JSON.stringify(cartLol));
+  carting();
+});
--- js/detail.js
+++ js/detail.js
@@ -5,113 +5,115 @@ let price = detail[1].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 // 상세페이지 내용
 let info = `<div class="detail_div row">
               <div class="product_img">
-                <img src="./images/JSON/${detail[2]}" alt="메인상품이미지" />
+                <img src="./images/JSON/${detail[2]}" alt="${detail[0]}" />
               </div>
-              <div class="product_ifm">
-                <div class="title_store">
-                  <a href="#"><span class="accent">리그오브레전드</span>
-                  <span class="accent">
-                      <i class="fa-solid fa-circle-check"></i> 공식</span>
-                  </a>
-                  <a href="index.html">
-                  <span><i class="fa-solid fa-house"></i> 라이엇스토어</span>
-                  </a>
-                </div>
-                <div class="title_name1">
-                  <h2>${detail[0]}</h2>
-                </div>
-                <div class="title_price">
-                  <div class="price">
-                    <span>${price}</span>
+              <form action="#" method="post" class="det_info">
+                <div class="product_ifm">
+                  <div class="title_store">
+                    <a href="#"><span class="accent">리그오브레전드</span>
+                    <span class="accent">
+                        <i class="fa-solid fa-circle-check"></i> 공식</span>
+                    </a>
+                    <a href="index.html">
+                    <span><i class="fa-solid fa-house"></i> 라이엇스토어</span>
+                    </a>
                   </div>
-                  <div class="icon">
-                  <span><i class="fa-regular fa-heart"></i></span>
-                  <span><i class="fa-solid fa-share-nodes"></i></span>
+                  <div class="title_name1">
+                    <h2>${detail[0]}</h2>
                   </div>
-                </div>
-                <div class="title_coupon">
-                  <span>이 상품에 적용 가능한<span class="accent_red">5% 할인</span> 쿠폰이 있어요</span>
-                  <span class="coupon1">쿠폰받기 <i class="fa-solid fa-download"></i></span>
-                </div>
-                <div class="title_namebox">
-                  <div class="title_day">
-                    <div class="title_daytext">
-                      <span><i class="fa-solid fa-truck-fast"></i> 내일 8/22(화) 출발 예정
-                      </span>
+                  <div class="title_price">
+                    <div class="price">
+                      <span>${price}</span>
                     </div>
-                    <div class="title_daybox">
-                      <ul>
-                        <li>
-                          <div class="dayday">
-                            <div class="dday">
-                              <span class="dayday1">도착확률정보</span>
-                            </div>
-                            <div class="ddday">
-                              <span class="dayday2">8/22(화) 도착</span>
-                              <progress class="gauge" value="78" max="100"></progress>
-                              <span class="dayday2">78%</span>
-                            </div>
-                          </div>
-                            <div class="dayinfo">
-                              <span>판매자의 평균 배송기간으로 예측되었습니다 (영업일 기준,주말/공휴일 제외)</span>
-                            </div>
-                        </li>
-                      </ul>
+                    <div class="icon">
+                    <span><i class="fa-regular fa-heart"></i></span>
+                    <span><i class="fa-solid fa-share-nodes"></i></span>
                     </div>
                   </div>
-                  <div class="title_name">
-                    <span class="title_name1">배송비 주문시 결제</span>
-                    <span>CJ택배</span>
+                  <div class="title_coupon">
+                    <span>이 상품에 적용 가능한<span class="accent_red">5% 할인</span> 쿠폰이 있어요</span>
+                    <span class="coupon1">쿠폰받기 <i class="fa-solid fa-download"></i></span>
                   </div>
-                  <div class="title_name">
-                    <span>스마일카드 <span class="accent">최대 2%</span> 캐시적립</span>
-                  </div>
-                  <div class="title_name">
-                    <span>신세계포인트 적립</span>
-                  </div>
-                  <div class="title_name">
-                    <span><i class="fa-solid fa-credit-card"></i> 결제수단 즉시할인<span class="accent">
-                    최대 10,000원</span></span>
-                  </div>
-                  <div class="title_name">
-                    <span><i class="fa-solid fa-credit-card"></i> 카드무이자</span>
-                    <span><i class="fa-regular fa-credit-card"></i> 카드 추가혜택</span>
-                  </div>
-                </div>
-                  <div class="banner">
-                    <img src="./images/DETAIL/배너.jpg" alt="배너" />
-                  </div>
-                  <div class="result">
-                    <div class="item_number">
-                      <span>수량</span>
-                    </div>
-                    <div class="total">
-                      <div class="item_result">
-                        <div class="minus"><button class="item_minus">-</button></div>
-                        <div class="input"><input type="text" value="1" autocomplete="off" class="item_input" /></div>
-                        <div class="plus"><button class="item_plus">+</button></div>
+                  <div class="title_namebox">
+                    <div class="title_day">
+                      <div class="title_daytext">
+                        <span><i class="fa-solid fa-truck-fast"></i> 내일 8/22(화) 출발 예정
+                        </span>
                       </div>
-                      <div class="price_result">
-                        <span>${price}</span>
+                      <div class="title_daybox">
+                        <ul>
+                          <li>
+                            <div class="dayday">
+                              <div class="dday">
+                                <span class="dayday1">도착확률정보</span>
+                              </div>
+                              <div class="ddday">
+                                <span class="dayday2">8/22(화) 도착</span>
+                                <progress class="gauge" value="78" max="100"></progress>
+                                <span class="dayday2">78%</span>
+                              </div>
+                            </div>
+                              <div class="dayinfo">
+                                <span>판매자의 평균 배송기간으로 예측되었습니다 (영업일 기준,주말/공휴일 제외)</span>
+                              </div>
+                          </li>
+                        </ul>
                       </div>
                     </div>
-                    <div class="total_price">
-                      <div>총 금액</div>
-                      <div class="reprice">${price}</div>
+                    <div class="title_name">
+                      <span class="title_name1">배송비 주문시 결제</span>
+                      <span>CJ택배</span>
+                    </div>
+                    <div class="title_name">
+                      <span>스마일카드 <span class="accent">최대 2%</span> 캐시적립</span>
+                    </div>
+                    <div class="title_name">
+                      <span>신세계포인트 적립</span>
+                    </div>
+                    <div class="title_name">
+                      <span><i class="fa-solid fa-credit-card"></i> 결제수단 즉시할인<span class="accent">
+                      최대 10,000원</span></span>
+                    </div>
+                    <div class="title_name">
+                      <span><i class="fa-solid fa-credit-card"></i> 카드무이자</span>
+                      <span><i class="fa-regular fa-credit-card"></i> 카드 추가혜택</span>
                     </div>
                   </div>
-                  <div class="btn">
-                    <button type="button" class="gift">
-                    <i class="fa-solid fa-gift"></i> 선물하기
-                    </button>
-                    <button type="button" class="cart">
-                    <i class="fa-solid fa-cart-shopping"></i> 장바구니
-                    </button>
-                    <button type="button" class="buy">
-                    <i class="fa-regular fa-credit-card"></i> 구매하기
-                    </button>
-                  </div>
-              </div>
+                    <div class="banner">
+                      <img src="./images/DETAIL/배너.jpg" alt="배너" />
+                    </div>
+                    <div class="result">
+                      <div class="item_number">
+                        <span>수량</span>
+                      </div>
+                      <div class="total">
+                        <div class="item_result">
+                          <div class="minus"><button type="button" class="item_minus">-</button></div>
+                          <div class="input"><input type="text" value="1" autocomplete="off" class="item_input" /></div>
+                          <div class="plus"><button type="button" class="item_plus">+</button></div>
+                        </div>
+                        <div class="price_result">
+                          <span>${price}</span>
+                        </div>
+                      </div>
+                      <div class="total_price">
+                        <div>총 금액</div>
+                        <div class="reprice">${price}</div>
+                      </div>
+                    </div>
+                    <div class="btn">
+                      <button type="submit" class="gift">
+                      <i class="fa-solid fa-gift"></i> 선물하기
+                      </button>
+                      <button type="submit" class="cart">
+                      <i class="fa-solid fa-cart-shopping"></i> 장바구니
+                      </button>
+                      <button type="submit" class="buy">
+                      <i class="fa-regular fa-credit-card"></i> 구매하기
+                      </button>
+                    </div>
+                </div>
+                </form>
               </div>
               <div class="tab">
                 <ul>
@@ -175,3 +177,26 @@ function getTotal() {
   total = quantity * detail[1];
   $(".reprice").text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 }
+
+$("body").on("submit", ".det_info", function (e) {
+  e.preventDefault();
+  let btn_class = $(document.activeElement).attr("class");
+  let newLol = {
+    name: detail[0],
+    price: detail[1],
+    image: detail[2],
+    quantity: quantity,
+  };
+  let itemLol = JSON.parse(localStorage.getItem("lolItem"));
+  if (itemLol == null) {
+    itemLol = [];
+  }
+  itemLol.push(newLol);
+  localStorage.setItem("lolItem", JSON.stringify(itemLol));
+
+  if (btn_class == "cart") {
+    location.href = "./cart.html";
+  } else {
+    location.href = "./buy.html";
+  }
+});
